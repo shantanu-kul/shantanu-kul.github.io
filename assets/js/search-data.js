d5c43a64267b893ade41568e992ae949cc852e69
shantanu-kul <153193584+shantanu-kul@users.noreply.github.com>
2026-03-27 02:20:45 +0000

    Deploying to gh-pages from @ shantanu-kul/shantanu-kul.github.io@5ead70eebdfed55ce91792e00c938613c6d7c73d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "Last updated: 2 June 2025",
+          description: "Last updated: 27 March 2026",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
